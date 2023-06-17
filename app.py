ba4f7409c163b4f7989f8722ecca2db80b5754fe
MattBiddick <matthew.biddick@icloud.com>
2023-06-17 13:48:59 +1200

    Added apple graphic

--- app.py
+++ app.py
@@ -64,7 +64,8 @@ class FRUIT:
     
     def draw_fruit(self):
         fruit_rect = pygame.Rect(self.pos.x * cell_size, self.pos.y * cell_size, cell_size, cell_size)
-        pygame.draw.rect(screen, (200,50,50), fruit_rect)
+        screen.blit(apple,fruit_rect)
+        #pygame.draw.rect(screen, (200,50,50), fruit_rect)
     
     def randomize(self):
         self.x = random.randint(0, cell_number - 1)
@@ -77,6 +78,7 @@ cell_size = 40
 cell_number = 20
 screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
 clock = pygame.time.Clock()
+apple = pygame.image.load('Graphics/apple.png').convert_alpha()
 
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 150)
