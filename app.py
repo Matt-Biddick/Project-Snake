c57b88f6d5d1277c462760f3d14e1628103904a0
MattBiddick <matthew.biddick@icloud.com>
2023-06-17 12:08:08 +1200

    Created fruit class

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-import pygame, sys
+import pygame, sys, random
+from pygame.math import Vector2
+
+class FRUIT:
+    def __init__(self):
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
+        self.pos = Vector2(self.x,self.y)
+    
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(self.pos.x * cell_size, self.pos.y * cell_size, cell_size, cell_size)
+        pygame.draw.rect(screen, (200,50,50), fruit_rect)
 
 # Initialize the pygame module and set tickrate
 pygame.init()
-screen = pygame.display.set_mode((400, 500))
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
 clock = pygame.time.Clock()
+
+fruit = FRUIT()
 
 # Main game loop
 while True:
@@ -11,6 +26,9 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    
+    screen.fill((150,150,150))
+    fruit.draw_fruit()
     pygame.display.update()
     clock.tick(60)
     
